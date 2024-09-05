90441c9e1ae8cf7c87a1df77afb995deffcce3eb
Avinash Vishwakarma <avinash@geoiq.io>
2024-09-05 23:37:32 +0530

    Improvement related to allowing host

--- movie_app/settings.py
+++ movie_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_h%vkhhq-s2!7%&zbdxcis5^zs=m*mdn)^siw_ve#d1_w_ft(k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
